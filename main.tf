5e3deaa841cefe29125913a2acced2177ec83639
saketVishwakarma <svsaket123@gmail.com>
2025-07-26 17:09:20 +0530

    adding vm size

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_virtual_machine" "vmtest" {
   location                      = azurerm_resource_group.test_net.location
   resource_group_name           = azurerm_resource_group.test_net.name
   network_interface_ids         = [azurerm_network_interface.main.id]
-  vm_size                       = "Standard_DS1_v2"
+  vm_size                       = "Standard_B2as_v2"
   delete_os_disk_on_termination = true
 
   storage_os_disk {
